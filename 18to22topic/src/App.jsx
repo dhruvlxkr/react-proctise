bc008009e506c13d42ffb6265e44ba44b0395792
dhruvlxkr <dhruvlxkr123@gmail.com>
2026-05-25 18:27:49 +0530

    changes

---
+++ 18to22topic/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+  return <div>
+    <div>Healthy Foods</div>
+     <ul class="list-group">
+    <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+</div>  
+  
+}
+
+export default App
